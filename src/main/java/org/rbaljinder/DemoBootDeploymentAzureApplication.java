a57ada268afccc4bc3ba8f764531c56379861b73
rbaljinder <tobaljinder@gmail.com>
2016-08-14 18:48:36 -0400

    Initial commit

---
+++ src/main/java/org/rbaljinder/DemoBootDeploymentAzureApplication.java
@@ -0,0 +1,12 @@
+package org.rbaljinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootDeploymentAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootDeploymentAzureApplication.class, args);
+	}
+}
